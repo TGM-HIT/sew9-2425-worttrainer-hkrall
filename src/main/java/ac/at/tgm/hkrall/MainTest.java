3193d56853f2d5fedf81c958ae8ec334566e4ff0
hkrall <hkrall@student.tgm.ac.at>
2025-01-09 15:12:48 +0100

    Laden für SaveLoad + Testing
    Die Funktion laden in die Klasse SaveLoad integriert und beide Funktionen getestet

--- src/main/java/ac/at/tgm/hkrall/MainTest.java
+++ src/main/java/ac/at/tgm/hkrall/MainTest.java
@@ -1,50 +1,10 @@
 package ac.at.tgm.hkrall;
 
+import java.io.IOException;
+
 public class MainTest {
 
     public static void main(String[] args) {
-
-        /**
-        WordEntry x= new WordEntry("Hund","https://www.bmel.de/SharedDocs/Bilder/DE/_Tiere/Haus-Zootiere/tierschutz-hunde.jpg?__blob=portrait&v=3");
-        System.out.println(x.toString());
-
-        WordEntry x2= new WordEntry("Hund","hundUrl");
-        System.out.println(x2.toString()+"\n");
-
-
-        //List tests
-        System.out.println("List Test:");
-        WordList list1=new WordList();
-        list1.addWord(x);
-        list1.addWord(x2);
-
-       //TODO System.out.println(list1.getWord(0).toString());
-        System.out.print(list1.toString());
-       //TODO     System.out.println("Delete Word: "+list1.deleteWord(1).toString());
-
-
-        System.out.println("WordTrainer Test:");
-        WordTrainer trainer1= new WordTrainer(list1);
-        System.out.println(trainer1.getRandomWord().toString());
-
-
-
-        System.out.println("\n\nNew bigger List");
-        WordEntry entry1= new WordEntry("Hund","https://www.bmel.de/SharedDocs/Bilder/DE/_Tiere/Haus-Zootiere/tierschutz-hunde.jpg?__blob=portrait&v=3");
-        WordEntry entry2 = new WordEntry("Katze", "https://image.geo.de/34423086/t/u8/v1/w1440/r0/-/katze-as-97589769.jpg");
-        WordEntry entry3 = new WordEntry("Vogel", "https://image.geo.de/30140936/t/bF/v3/w1440/r1/-/schuhschnabel-vogel-geolino2-jpg--80202-.jpg");
-        WordEntry entry4 = new WordEntry("Fisch", "https://blog.the-british-shop.at/fileadmin/user_upload/fishy.jpg");
-        WordEntry entry5 = new WordEntry("Maus", "https://media.os.fressnapf.com/cms/2020/07/Ratgeber_Kleintier_maus_1200x527.jpg?t=seoimg_703");
-
-        WordList list= new WordList();
-        list.addWord(entry1);list.addWord(entry2);list.addWord(entry3);list.addWord(entry4);list.addWord(entry5);
-        System.out.print(list.toString());
-
-        WordTrainer trainer= new WordTrainer(list);
-        System.out.println("\nRandom Word: \n"+trainer.getRandomWord().toString());
-
-        System.out.println("checkWord: \n"+trainer.checkWord("Hund"));
-         */
 
         // Test WordEntry
         System.out.println("WordEntry Test:");
@@ -75,11 +35,6 @@ public class MainTest {
 
         // Test WordTrainer
         System.out.println("\nWordTrainer Test:");
-     /*     WordTrainer trainer1 = new WordTrainer(list1);
-        WordEntry randomWord = trainer1.getRandomWord();
-        System.out.println("Random Word:");
-        System.out.println(randomWord != null ? randomWord : "No words available");*/
-
         System.out.println("\nNew bigger List Test:");
         WordEntry entry1 = new WordEntry("Hund", "https://www.bmel.de/SharedDocs/Bilder/DE/_Tiere/Haus-Zootiere/tierschutz-hunde.jpg?__blob=portrait&v=3");
         WordEntry entry2 = new WordEntry("Katze", "https://image.geo.de/34423086/t/u8/v1/w1440/r0/-/katze-as-97589769.jpg");
@@ -96,8 +51,11 @@ public class MainTest {
         System.out.println(biggerList);
 
         WordTrainer biggerTrainer = new WordTrainer(biggerList);
-        WordEntry randomBiggerWord = biggerTrainer.getRandomWord();
+        biggerTrainer.setCorrectTrys(5);
+        biggerTrainer.setAllTrys(10);
+
         System.out.println("\nRandom Word from bigger list:");
+        WordEntry randomBiggerWord = biggerTrainer.getRandomWord();
         System.out.println(randomBiggerWord != null ? randomBiggerWord : "No words available");
 
         System.out.println("Check word 'Hund':");
@@ -105,6 +63,28 @@ public class MainTest {
 
         System.out.println("Check word 'Katze':");
         System.out.println(biggerTrainer.checkWord("Katze") ? "Correct" : "Incorrect");
-    }
 
+        // Test SaveLoad.save
+        System.out.println("\nTesting SaveLoad.save...");
+        String fileName = "wordTrainerData.txt";
+        try {
+            SaveLoad.save(biggerTrainer, fileName);
+            System.out.println("Daten erfolgreich in '" + fileName + "' gespeichert.");
+        } catch (IOException e) {
+            System.err.println("Fehler beim Speichern der Daten: " + e.getMessage());
+        }
+
+        // Test SaveLoad.laden
+        System.out.println("\nTesting SaveLoad.laden...");
+        try {
+            WordTrainer loadedTrainer = SaveLoad.laden(fileName);
+            System.out.println("Daten erfolgreich aus '" + fileName + "' geladen.");
+            System.out.println("Geladene Wortliste:");
+            System.out.println(loadedTrainer.getWordlist());
+            System.out.println("Richtige Versuche: " + loadedTrainer.getCorrectTrys());
+            System.out.println("Gesamtversuche: " + loadedTrainer.getAllTrys());
+        } catch (IOException e) {
+            System.err.println("Fehler beim Laden der Daten: " + e.getMessage());
+        }
+    }
 }
--- src/main/java/ac/at/tgm/hkrall/SaveLoad.java
+++ src/main/java/ac/at/tgm/hkrall/SaveLoad.java
@@ -1,9 +1,7 @@
 package ac.at.tgm.hkrall;
 
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class SaveLoad {
 
@@ -22,5 +20,35 @@ public class SaveLoad {
             writer.newLine();
         }
     }
+
+    public static WordTrainer laden(String fileName) throws IOException {
+        WordList wortListe = new WordList();
+        int richtigeVersuche = 0;
+        int gesamtVersuche = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("RichtigeVersuche:")) {
+                    richtigeVersuche = Integer.parseInt(line.substring("RichtigeVersuche:".length()));
+                } else if (line.startsWith("GesamtVersuche:")) {
+                    gesamtVersuche = Integer.parseInt(line.substring("GesamtVersuche:".length()));
+                } else {
+                    String[] parts = line.split(",");
+                    if (parts.length == 2) {
+                        String wort = parts[0];
+                        String url = parts[1];
+                        wortListe.addWord(new WordEntry(wort, url));
+                    }
+                }
+            }
+        }
+
+        WordTrainer wortTrainer = new WordTrainer(wortListe);
+        wortTrainer.setCorrectTrys(richtigeVersuche);
+        wortTrainer.setAllTrys(gesamtVersuche);
+        return wortTrainer;
+    }
+
 
 }
